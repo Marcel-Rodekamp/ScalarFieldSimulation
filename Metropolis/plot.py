2085a0cfef14a335c5f8191c2021dd1f462afe31
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2019-11-23 11:39:56 +0100

    added plot expVal

--- Metropolis/plot.py
+++ Metropolis/plot.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+
+SHOWPLOT = True 
 
 def average(lattice, latticeSize):
     av = 0
@@ -8,29 +11,36 @@ def average(lattice, latticeSize):
 
     return av / latticeSize
 
-def plotLattice():
-    configName = [0, 240000, 400000]
-
+def plotLattice(latticeSize = 500, configNum = [0,240000,400000]):
+    xData = range(0,latticeSize,1)
 
     # Plot the lattice
-    for n in configName:
-        data = np.loadtxt("Confs/config_" + str(n) + ".lat")
-
-        xData = [] #data[:][0]
-        yData = [] #data[:][1]
-
-        for i in range(0,int(data.size/3)):
-            xData.append(data[i][0])
-            yData.append(data[i][1])
-
+    for n in configNum:
         lab = "Cooling Steps = " + str(n)
-        plt.plot(xData,yData, label = lab)
+        plt.plot(xData,np.loadtxt("Confs/config_" + str(n) + ".lat", usecols=1), label = lab)
 
-    plt.ylabel("Configuration Value")
-    plt.xlabel("Lattice Site")
+    plt.ylabel("x")
+    plt.xlabel("Site")
     plt.legend()
     plt.savefig("conf_metropolis.pdf")
-    plt.show()
+    if SHOWPLOT: 
+        plt.show()
     plt.clf()
 
+def plotExpVal(latticeSize = 500, configNum = range(0,400000,10000)):
+    yData = []
 
+    for n in configNum:
+        yData.append(average(np.loadtxt("Confs/config_"+str(n)+".lat",usecols=1),latticeSize))    
+
+    plt.plot(configNum,yData)
+    plt.ylabel("<x>")
+    plt.xlabel("#sweeps")
+    plt.savefig("expVal_metropolis.pdf")
+    if SHOWPLOT:
+        plt.show()
+    plt.clf()    
+
+allConfNum = range(0,400000,10000)
+plotExpVal()
+#plotLattice()
