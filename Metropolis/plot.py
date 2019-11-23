c84ed8214336e43068ab7e6065375ddeaaa816e3
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2019-11-23 10:57:05 +0100

    First step for generalization

--- Metropolis/plot.py
+++ Metropolis/plot.py
@@ -1,26 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-configName = [0, 240000, 400000]
 
-# Plot the lattice
-for n in configName:
-    data = np.loadtxt("Confs/config_" + str(n) + ".lat")
-
-    xData = [] #data[:][0]
-    yData = [] #data[:][1]
-
-    for i in range(0,int(data.size/3)):
-        xData.append(data[i][0])
-        yData.append(data[i][1])
-
-    lab = "Cooling Steps = " + str(n)
-    plt.plot(xData,yData, label = lab)
+def plotLattice():
+    configName = [0, 240000, 400000]
 
 
-plt.ylabel("Configuration Value")
-plt.xlabel("Lattice Site")
-plt.legend()
-plt.show()
+    # Plot the lattice
+    for n in configName:
+        data = np.loadtxt("Confs/config_" + str(n) + ".lat")
 
-#plt.savefig("conf_metropolis.pdf")
+        xData = [] #data[:][0]
+        yData = [] #data[:][1]
+
+        for i in range(0,int(data.size/3)):
+            xData.append(data[i][0])
+            yData.append(data[i][1])
+
+        lab = "Cooling Steps = " + str(n)
+        plt.plot(xData,yData, label = lab)
+
+    plt.ylabel("Configuration Value")
+    plt.xlabel("Lattice Site")
+    plt.legend()
+    plt.savefig("conf_metropolis.pdf")
+    plt.show()
+    plt.clf()
+
+
+plotLattice()
